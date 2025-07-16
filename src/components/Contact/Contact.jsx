973453c431c3ee5effbad1ed93a101e2d914c1e5
sonuchaurasiya7633 <sonukumar763303@gmail.com>
2025-07-16 19:47:16 +0530

    fisrt

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,10 +12,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_axbtt7a",  // Replace with your EmailJS Service ID
-        "template_1ziboq3",  // Replace with your EmailJS Template ID
+        "service_yuk9rgs",  // Replace with your EmailJS Service ID
+        "template_v6gn2g7",  // Replace with your EmailJS Template ID
         form.current,
-        "Rz7W9pVF0HdDryNNL"  // Replace with your EmailJS Public Key
+        "wJvTC3Wn0sZTvvsFC"  // Replace with your EmailJS Public Key
       )
       .then(
         () => {
