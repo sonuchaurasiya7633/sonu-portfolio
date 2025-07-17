d02eecd0cfe88fb54baecc8e16bf04d322453fcf
sonuchaurasiya7633 <sonukumar763303@gmail.com>
2025-07-17 08:06:13 +0530

    updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1LY09lpikajQgTQjeUjxre9QYU0astH18/view?usp=drivesdk"
+            href="/SonuResume.pdf"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
